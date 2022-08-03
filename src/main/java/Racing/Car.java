6fb007374da5d27fb006890f70f796b8e6136b92
inuit57 <inuit57@naver.com>
2022-08-03 13:51:01 +0900

    feat : 자동차 정보 출력하기

--- src/main/java/Racing/Car.java
+++ src/main/java/Racing/Car.java
@@ -4,8 +4,25 @@ public class Car {
     private final String name;
     private int move ;
 
-    public Car(String name) {
+    protected Car(String name) {
         this.name = name ;
         move = 1;
+    }
+
+    public void setMove(int move) {
+        this.move = move;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(this.name);
+        sb.append(" : ");
+        for(int i=0; i< this.move; i++){
+            sb.append("-");
+        }
+
+        return sb.toString();
     }
 }
--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -1,17 +1,21 @@
 import Racing.Car;
 import Racing.CarFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
 
+    private final CarFactory carFactory = new CarFactory();
+
     @Test
-    public void 자동차_생성하기(){
+    @DisplayName("차량생성 테스트")
+    public void createCarTest(){
+
         //given
         String longName = "longNameCar";
         String shortName = "car12";
-        CarFactory carFactory = new CarFactory();
 
         //when
         Car longNameCar = carFactory.createCar(longName);
@@ -20,5 +24,18 @@ public class RacingTest {
         //then
         assertThat(longNameCar).isNull();
         assertThat(shortNameCar).isNotNull();
+    }
+    
+    @Test
+    @DisplayName("차량정보 출력 테스트")
+    public void carInfoPrintTest() {
+        //given
+        Car car = carFactory.createCar("test");
+
+        //when
+        System.out.println(car);
+
+        car.setMove(3);
+        System.out.println(car);
     }
 }
