4d01c45a7a2adfbe25dfe149127ca50d1adaa8d1
inuit57 <inuit57@naver.com>
2022-08-03 16:08:27 +0900

    docs: 함수 설명문 추가

--- src/main/java/Racing/Car.java
+++ src/main/java/Racing/Car.java
@@ -1,6 +1,7 @@
 package Racing;
 
 public class Car {
+    public static final int CAN_MOVE_NUM = 4;
     private final String name;
     private int move ;
 
@@ -13,6 +14,7 @@ public class Car {
         this.move = move;
     }
 
+    // 최댓 이동거리 판정을 위해서 필요하다.
     public int getMove() {
         return move;
     }
@@ -34,8 +36,14 @@ public class Car {
         return sb.toString();
     }
 
+    /**
+     *  이동 가능 여부를 확인한다.
+     *  move를 증가시킨다.
+     * @param random 랜덤으로 입력된 수치값
+     * @return 이동가능 여부를 리턴
+     */
     public boolean isMove(int random) {
-        if(random >= 4){
+        if(random >= CAN_MOVE_NUM){
             this.move++;
             return true;
         }
